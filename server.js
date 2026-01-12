a2b99c599721e90e13aeead73c76a75f2b8781d0
ritesh5001 <rgiri5001@gmail.com>
2026-01-13 00:46:14 +0530

    Refactor code structure for improved readability and maintainability

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('API is running... Welcome to Explore Fusion!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

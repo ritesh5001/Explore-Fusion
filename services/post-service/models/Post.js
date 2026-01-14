20227d9134c65021f8e7e400aa54f5776af2a148
ritesh5001 <rgiri5001@gmail.com>
2026-01-15 00:12:27 +0530

    feat: implement image upload functionality for posts with associated metadata

--- services/post-service/models/Post.js
+++ services/post-service/models/Post.js
@@ -6,6 +6,8 @@ const postSchema = new mongoose.Schema(
     content: { type: String, required: true },
     location: { type: String, required: true },
     imageUrl: { type: String, trim: true },
+    imageFileId: { type: String, trim: true },
+    imageFolder: { type: String, trim: true },
     author: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',

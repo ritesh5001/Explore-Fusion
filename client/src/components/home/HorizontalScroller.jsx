4d9ec6cc7038f60409af6f48a98dcca7ef1225f9
ritesh5001 <rgiri5001@gmail.com>
2026-01-16 20:32:36 +0530

    feat: Update multiplier in useVerticalToHorizontalScroll for enhanced scrolling responsiveness

--- client/src/components/home/HorizontalScroller.jsx
+++ client/src/components/home/HorizontalScroller.jsx
@@ -92,7 +92,7 @@ export default function HorizontalScroller({
 		};
 	}, []);
 
-	useVerticalToHorizontalScroll(scrollerRef, { multiplier: 2.5 });
+	useVerticalToHorizontalScroll(scrollerRef, { multiplier: 3 });
 
 	useEffect(() => {
 		const el = scrollerRef.current;
